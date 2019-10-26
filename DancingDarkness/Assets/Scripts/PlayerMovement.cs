9f01ab229c708938b509c7da841616600842ebf4
mfngoi <56236633+mfngoi@users.noreply.github.com>
2019-10-26 15:23:08 -0700

    changed duck prefab to fit BenScene

--- DancingDarkness/Assets/Scripts/PlayerMovement.cs
+++ DancingDarkness/Assets/Scripts/PlayerMovement.cs
@@ -14,37 +14,37 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        if(transform.position.x == 0.5)
+        if(transform.position.x == -0.5)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                transform.position = new Vector2(-0.5f, -1.5f);
+                transform.position = new Vector2(-1.5f, -2.5f);
             }
             else if (Input.GetKeyDown(KeyCode.D))
             {
-                transform.position = new Vector2(1.5f, -1.5f);
+                transform.position = new Vector2(0.5f, -2.5f);
             }
         }
-        else if(transform.position.x == -0.5)
+        else if(transform.position.x == -1.5)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                transform.position = new Vector2(-0.5f, -1.5f);
+                transform.position = new Vector2(-1.5f, -2.5f);
             }
             else if (Input.GetKeyDown(KeyCode.D))
             {
-                transform.position = new Vector2(0.5f, -1.5f);
+                transform.position = new Vector2(-0.5f, -2.5f);
             }
         }
-        else if (transform.position.x == 1.5)
+        else if (transform.position.x == 0.5)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                transform.position = new Vector2(0.5f, -1.5f);
+                transform.position = new Vector2(-0.5f, -2.5f);
             }
             else if (Input.GetKeyDown(KeyCode.D))
             {
-                transform.position = new Vector2(1.5f, -1.5f);
+                transform.position = new Vector2(0.5f, -2.5f);
             }
         }
     }
