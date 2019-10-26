0f369d3feae2108feadeb30199e17c85cab73378
mfngoi <56236633+mfngoi@users.noreply.github.com>
2019-10-26 15:14:14 -0700

    player movement

--- DancingDarkness/Assets/Scripts/PlayerMovement.cs
+++ DancingDarkness/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(transform.position.x == 0.5)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = new Vector2(-0.5f, -1.5f);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = new Vector2(1.5f, -1.5f);
+            }
+        }
+        else if(transform.position.x == -0.5)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = new Vector2(-0.5f, -1.5f);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = new Vector2(0.5f, -1.5f);
+            }
+        }
+        else if (transform.position.x == 1.5)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = new Vector2(0.5f, -1.5f);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = new Vector2(1.5f, -1.5f);
+            }
+        }
     }
 }
