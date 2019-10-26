86a1edfe217bbb534a1c1cc9f873d94b9957b083
Benjaminung1 <56207323+Benjaminung1@users.noreply.github.com>
2019-10-26 16:03:33 -0700

    Added Enemy Script

---
+++ DancingDarkness/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject HealthBar;
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    Vector3 position;
+    private void Start()
+    {
+        position = this.transform.position;
+        HealthBar = GameObject.Find("Bar");
+        Move();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Move()
+    {
+        while (true)
+        {
+            StartCoroutine("EnemyMovement");
+        }
+    }
+
+    IEnumerator EnemyMovement()
+    {
+        position = position - new Vector3(0, 1, 0);
+        this.DamageCheck();
+        yield return new WaitForSeconds(1); 
+    }
+
+    void DamageCheck()
+    {
+        if(position.y < -2)
+        {
+            HealthBar.GetComponent<HealthBar>().TakeDamage();
+            Destroy(this);
+        }
+
+    }
+}
