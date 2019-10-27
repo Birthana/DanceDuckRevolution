1fa3f555c6e07093e838369b8a44c54fe170b947
Benjaminung1 <56207323+Benjaminung1@users.noreply.github.com>
2019-10-26 21:25:13 -0700

    Fixed buttons

--- DancingDarkness/Assets/Scripts/ProjectileCollider.cs
+++ DancingDarkness/Assets/Scripts/ProjectileCollider.cs
@@ -11,13 +11,16 @@ public class ProjectileCollider: MonoBehaviour {
 
     IEnumerator Death(Collision2D collision)
     {
-        if (collision.collider.tag == "Enemy")
+        if (collision.collider.tag == "Enemy" || collision.collider.tag == "Health")
         {
             collision.gameObject.GetComponent<Animator>().SetBool("IsDead", true);
             collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             collision.gameObject.GetComponent<EnemyBehavior>().moving = false;
             this.GetComponent<SpriteRenderer>().enabled = false;
-            yield return new WaitForSeconds(0.3f);
+            if (collision.collider.tag != "Health")
+            {
+                yield return new WaitForSeconds(0.3f);
+            }
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
         }
