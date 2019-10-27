6a920ad90633da063ae27d9c23a4356227ca4327
Benjaminung1 <56207323+Benjaminung1@users.noreply.github.com>
2019-10-26 18:39:05 -0700

    Made enemy variants appear

--- DancingDarkness/Assets/Scripts/EnemySpawner.cs
+++ DancingDarkness/Assets/Scripts/EnemySpawner.cs
@@ -8,10 +8,13 @@ public class EnemySpawner : MonoBehaviour
     //spawn
     //fall
     //deal damage
+    public Sprite[] images;
+
     float SpawnHeight = 3.5f;
     float[] position = new float[3];
     public GameObject enemy;
-
+    int counter = 0;
+    
     private void Start()
     {
         Spawn();
@@ -19,11 +22,22 @@ public class EnemySpawner : MonoBehaviour
 
     public void Spawn()
     {
-        //it's not random. it always spawn on the left most column
-        position[0] = Random.Range(1,4) - 2.5f;
-        position[1] = SpawnHeight;
-        position[2] = 0;
-        //check tags instead of class?
-        Instantiate(enemy, new Vector3(position[0],position[1],position[2]), Quaternion.identity);
+        StartCoroutine("Spawning");
+
+    }
+
+    IEnumerator Spawning() {
+        while(counter < 10) {
+            position[0] = Random.Range(1, 4) - 2.5f;
+            position[1] = SpawnHeight;
+            position[2] = 0;
+            //check tags instead of class?
+            GameObject tempEnemy = Instantiate(enemy, new Vector3(position[0], position[1], position[2]), Quaternion.identity);
+            int EnemyVariant = Random.Range(0, 4);
+            //returns game object. = a variable that is the game object. use that game object variable to change 
+            tempEnemy.GetComponent<SpriteRenderer>().sprite = images[EnemyVariant];
+            counter++;
+            yield return new WaitForSeconds(2);
+        }
     }
 }
