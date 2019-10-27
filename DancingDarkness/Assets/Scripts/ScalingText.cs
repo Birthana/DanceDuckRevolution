5c5083e70cb243728707fa2aa82bb2cee4873d50
Benjaminung1 <56207323+Benjaminung1@users.noreply.github.com>
2019-10-26 20:54:24 -0700

    Fixed credit screen

---
+++ DancingDarkness/Assets/Scripts/ScalingText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalingText : MonoBehaviour
+{
+    int width;
+    int height;
+    void Start()
+    {
+        width = Screen.width;
+        height = Screen.height;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
