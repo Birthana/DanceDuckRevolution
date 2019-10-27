78e355a52797aafe7b25e1dbe8a2b4d228b3ea12
mfngoi <56236633+mfngoi@users.noreply.github.com>
2019-10-26 17:30:40 -0700

    Added projectile behavior prefab and initialization

---
+++ DancingDarkness/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public bool cooldown = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !cooldown)
+        {
+            Attack();
+            cooldown = true;
+        }
+    }
+
+    public void Attack()
+    {
+        StartCoroutine("Cooldown");
+    }
+
+    IEnumerator Cooldown()
+    {
+        Instantiate(projectile, this.GetComponent<Transform>().position, Quaternion.identity);
+        yield return new WaitForSeconds(2);
+        cooldown = false;
+    }
+
+}
