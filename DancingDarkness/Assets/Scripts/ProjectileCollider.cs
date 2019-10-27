42eece0431e28e1c6157e1738085908d31137897
Benjaminung1 <56207323+Benjaminung1@users.noreply.github.com>
2019-10-26 18:07:30 -0700

    Made collider and fixed spawner

--- DancingDarkness/Assets/Scripts/ProjectileCollider.cs
+++ DancingDarkness/Assets/Scripts/ProjectileCollider.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ProjectileCollider: MonoBehaviour {
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Before if statement");
         if(collision.collider.tag == "Enemy")
         {
+            Debug.Log("Inside if statement");
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
         }
+        Debug.Log("After if statement");
     }
+        
 }
