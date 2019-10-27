5a5c1dbef0147c7e685f6b45376f827aba1f9dfe
mfngoi <56236633+mfngoi@users.noreply.github.com>
2019-10-26 18:39:48 -0700

    add fonts and worked on projectile attacks

--- DancingDarkness/Assets/Scripts/PlayerAttack.cs
+++ DancingDarkness/Assets/Scripts/PlayerAttack.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
 
+    public SpriteRenderer sprite;
+    public Sprite[] dance = new Sprite[8];
+
     public GameObject projectile;
     public bool cooldown = false;
+
+    public GameObject attackText;
+    private string[] attackKey = { "Q", "W", "E", "R", "A", "S", "D", "F" };
+    public string presentAttack;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        presentAttack = attackKey[Random.Range(0, 8)];
+        attackText.GetComponent<Text>().text = presentAttack;
+        sprite = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -32,7 +42,8 @@ public class PlayerAttack : MonoBehaviour
 
     IEnumerator Cooldown()
     {
-        Instantiate(projectile, this.GetComponent<Transform>().position, Quaternion.identity);
+        GameObject attack = Instantiate(projectile, this.GetComponent<Transform>().position, Quaternion.identity);
+        attack.GetComponent<SpriteRenderer>().sprite = dance[0];
         yield return new WaitForSeconds(2);
         cooldown = false;
     }
