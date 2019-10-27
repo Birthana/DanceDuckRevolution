3871ca77169a79b7ec8ad757f333e0cb18fd030c
Benjaminung1 <56207323+Benjaminung1@users.noreply.github.com>
2019-10-26 20:19:03 -0700

    working on death animations

--- DancingDarkness/Assets/Scripts/EnemyBehavior.cs
+++ DancingDarkness/Assets/Scripts/EnemyBehavior.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public GameObject HealthBar;
+    public bool moving;
 
     // Start is called before the first frame update
     // Update is called once per frame
     private void Start()
     {
         HealthBar = GameObject.Find("Bar");
+        moving = true;
         Move();
     }
 
@@ -23,19 +25,25 @@ public class EnemyBehavior : MonoBehaviour
     {
         for (int i = 0; i < 6; i++)
         {
-            this.GetComponent<Transform>().position -= new Vector3(0, 1, 0);
-            DamageCheck();
-            yield return new WaitForSeconds(1);
+            if (moving)
+            {
+                this.GetComponent<Transform>().position -= new Vector3(0, 1, 0);
+                if (this.GetComponent<Transform>().position.y < -2.0f)
+                {
+                    HealthBar.GetComponent<HealthBar>().TakeDamage();
+                    this.GetComponent<Animator>().SetBool("IsDead", true);
+                    this.GetComponent<BoxCollider2D>().enabled = false;
+                    moving = false;
+                    yield return new WaitForSeconds(0.3f);
+                    Destroy(this.gameObject);
+                }
+                yield return new WaitForSeconds(1);
+            }
         }
     }
 
     void DamageCheck()
     {
-        if(this.GetComponent<Transform>().position.y < -2.0f)
-        {
-            HealthBar.GetComponent<HealthBar>().TakeDamage();
-            Destroy(this.gameObject);
-
-        }
+        
     }
 }
